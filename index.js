9a869355991ba98d13b3c2025a491454583c3338
prahal <vprahaladhan@gmail.com>
2021-03-09 09:17:44 +1100

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ document.getElementById('main').remove();
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
 newHeader.innerHTML = 'Prahal is the champion';
+document.appendChild(newHeader);
